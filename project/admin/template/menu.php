d147ff8c2aefb1ada30b31dc695b2e6367649f08
meliarosita <43667609+meliarosita@users.noreply.github.com>
2019-01-14 21:48:04 +0700

    Add files via upload

---
+++ project/admin/template/menu.php
@@ -0,0 +1,159 @@
+
+<div class="app-body">
+      <div class="sidebar">
+        <nav class="sidebar-nav">
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php?module=home">
+                <i class="nav-icon icon-home"></i> Dashboard
+                <span class="badge badge-primary">NEW</span>
+              </a>
+            </li>
+            <li class="nav-title">Biaya Kirim</li>
+            <li class="nav-item">
+              <a class="nav-link" href="admin.php?module=biayakirim">
+                <i class="nav-icon icon-globe"></i> Biaya Kirim</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="typography.html">
+                <i class="nav-icon icon-pencil"></i> Typography</a>
+            </li>
+            <li class="nav-title">Produk</li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-social-dropbox"></i> Produk</a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="admin.php?module=kategori">
+                    <i class="nav-icon icon-options"></i>kategori</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="admin.php?module=merek">
+                    <i class="nav-icon icon-options-vertical"></i>merek</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="admin.php?module=produk">
+                    <i class="nav-icon icon-grid"></i> produk</a>
+                </li>
+                
+              </ul>
+            </li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-cursor"></i> Buttons</a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="buttons/buttons.html">
+                    <i class="nav-icon icon-cursor"></i> Buttons</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="buttons/button-group.html">
+                    <i class="nav-icon icon-cursor"></i> Buttons Group</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="buttons/dropdowns.html">
+                    <i class="nav-icon icon-cursor"></i> Dropdowns</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="buttons/brand-buttons.html">
+                    <i class="nav-icon icon-cursor"></i> Brand Buttons</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="charts.html">
+                <i class="nav-icon icon-pie-chart"></i> Charts</a>
+            </li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-star"></i> Icons</a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="icons/coreui-icons.html">
+                    <i class="nav-icon icon-star"></i> CoreUI Icons
+                    <span class="badge badge-success">NEW</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="icons/flags.html">
+                    <i class="nav-icon icon-star"></i> Flags</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="icons/font-awesome.html">
+                    <i class="nav-icon icon-star"></i> Font Awesome
+                    <span class="badge badge-secondary">4.7</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="icons/simple-line-icons.html">
+                    <i class="nav-icon icon-star"></i> Simple Line Icons</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-bell"></i> Notifications</a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="notifications/alerts.html">
+                    <i class="nav-icon icon-bell"></i> Alerts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="notifications/badge.html">
+                    <i class="nav-icon icon-bell"></i> Badge</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="notifications/modals.html">
+                    <i class="nav-icon icon-bell"></i> Modals</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="widgets.html">
+                <i class="nav-icon icon-calculator"></i> Widgets
+                <span class="badge badge-primary">NEW</span>
+              </a>
+            </li>
+            <li class="divider"></li>
+            <li class="nav-title">Extras</li>
+            <li class="nav-item nav-dropdown">
+              <a class="nav-link nav-dropdown-toggle" href="#">
+                <i class="nav-icon icon-star"></i> Pages</a>
+              <ul class="nav-dropdown-items">
+                <li class="nav-item">
+                  <a class="nav-link" href="login.html" target="_top">
+                    <i class="nav-icon icon-star"></i> Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="register.html" target="_top">
+                    <i class="nav-icon icon-star"></i> Register</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="404.html" target="_top">
+                    <i class="nav-icon icon-star"></i> Error 404</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="500.html" target="_top">
+                    <i class="nav-icon icon-star"></i> Error 500</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item mt-auto">
+              <a class="nav-link nav-link-success" href="https://coreui.io" target="_top">
+                <i class="nav-icon icon-cloud-download"></i> Download CoreUI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-link-danger" href="https://coreui.io/pro/" target="_top">
+                <i class="nav-icon icon-layers"></i> Try CoreUI
+                <strong>PRO</strong>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      
+        <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+      </div>
+
+      
+     
+    
